36485752228a0a702e71c92b645b3bfb6c4e3ce0
MostafaSharaf <mostafa.sharaf@sap.com>
2021-08-19 20:38:04 +0400

    khara alek

---
+++ app.js
@@ -0,0 +1,12 @@
+////
+const express = require('express'), app = express();
+////
+getHTTPPostData();
+////
+function getHTTPPostData() {
+  app.listen(9000, function()
+  {
+    console.log("Hello World!");
+  });
+}
+//

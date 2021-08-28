38d9d920c427f7064ee2ce8e02c534e79b5a9b78
MostafaSharaf <mostafa.sharaf@sap.com>
2021-08-28 16:46:47 +0400

    commit_final

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const os = require('os');
 const PORT = process.env.PORT;
 //
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World Node app, listening at http://localhost:' + listener.address().port + ', at conatiner؛ ' + os.hostname);
 });
 //
 var listener = app.listen(PORT || 3000, () => {
-  console.log('Example app listening at http://localhost:' + listener.address().port + ', at conatiner؛ ' + os.hostname + '');
+  console.log('Example app listening at http://localhost:' + listener.address().port + ', at conatiner؛ ' + os.hostname);
 });
 //
